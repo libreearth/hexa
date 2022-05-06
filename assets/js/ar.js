d53a8bf5606f781006b37a3b8b1abb08ca0a934b
Gabriel Bellido Perez <gabrielete@gmail.com>
2022-05-06 09:08:18 +0200

    Fix aframe initialization

--- assets/js/ar.js
+++ assets/js/ar.js
@@ -2,7 +2,17 @@ import "aframe"
 
 arhook = {
   mounted() {
+    var aSceneEl = document.createElement("a-scene")
+    aSceneEl.setAttribute("embedded", true)
 
+    var boxEl = document.createElement("a-box")
+    boxEl.setAttribute("position", "-1 0.5 -3")
+    boxEl.setAttribute("rotation", "0 45 0")
+    boxEl.setAttribute("color", "#4CC3D9")
+    aSceneEl.appendChild(boxEl)
+
+    var arEl = document.querySelector("#ar-frame")
+    arEl.appendChild(aSceneEl)
   }
 }
 
