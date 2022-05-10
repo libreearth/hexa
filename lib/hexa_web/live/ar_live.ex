15172cc06ea9d16105a9e426c3d93a1cf4911375
Gabriel Bellido Perez <gabrielete@localhost.localdomain>
2022-05-10 15:51:03 +0000

    Add basis for Ar section

--- lib/hexa_web/live/ar_live.ex
+++ lib/hexa_web/live/ar_live.ex
@@ -1,23 +1,23 @@
 defmodule HexaWeb.ArLive do
   use HexaWeb, :live_view
-  
+
   alias HexaWeb.ArComponent
 
   def render(assigns) do
     ~H"""
     <.title_bar>
-      Ar exploration
+      <div id="towrite">Ar exploration</div>
     </.title_bar>
 
     <div class="max-w-3xl px-4 mx-auto mt-6">
-      <.live_component module={ArComponent} id="ar"/> 
+      <.live_component module={ArComponent} id="ar"/>
     </div>
     """
   end
-    
+
   def mount(_parmas, _session, socket) do
     {
-      :ok, 
+      :ok,
       socket
     }
   end
--- lib/hexa_web/live/components/ar_component.ex
+++ lib/hexa_web/live/components/ar_component.ex
@@ -4,7 +4,21 @@ defmodule HexaWeb.ArComponent do
   def render(assigns) do
     ~H"""
       <div id="ar-frame" phx-hook="Ar">
-        
+        <a-scene embedded vr-mode-ui="enabled: false"
+        arjs="sourceType: webcam; videoTexture: true; debugUIEnabled: false;">
+          <a-camera gps-camera="alert: true; positionMinAccuracy : 10;" rotation-reader/>
+          <a-box gps-entity-place="latitude: 37.3473279; longitude: -5.9735585;"
+            color="#4CC3D9"
+            position="0 0 0"/>
+
+          <a-box gps-entity-place="latitude: 39.136634; longitude: -6.842715;"
+            color="#4CC3D9"
+            position="0 0 0"/>
+
+          <a-box gps-entity-place="latitude: 37.331830; longitude: -5.9667812;"
+            color="#4CC3D9"
+            position="0 0 0"/>
+        </a-scene>
       </div>
     """
   end
